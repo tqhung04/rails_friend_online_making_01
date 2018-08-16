be16b7a4256fc174fe32d073e1de834fc99b86f8
TQHUNG04 <tqhung04@gmail.com>
2018-08-16 08:42:55 +0700

    fix report

--- app/controllers/conections_controller.rb
+++ app/controllers/conections_controller.rb
@@ -7,36 +7,32 @@ class ConectionsController < ApplicationController
       @conection = current_user.conections.new conection_params
       if @conection.save
         @user =User.find_by id: @conection.recipient_id
-        respond_to do |format|
-          format.html {redirect_to relationships_url}
-          format.js
-        end
+        conections_respond
       end
     end
   end
 
   def update
     @conection.update_attributes status: true
-    conversation = current_user.conversations.new recipient_id: @conection.sender_id
-    if conversation.save
-      @user = User.find_by id: @conection.recipient_id
-      respond_to do |format|
-        format.html {redirect_to relationships_url}
-        format.js
+    @user = User.find_by id: @conection.recipient_id
+    check_unique = Conversation.between(@conection.sender_id, @conection.recipient_id)
+    if check_unique.present?
+      conversation = current_user.conversations.new recipient_id: @conection.sender_id
+      if conversation.save
+        conections_respond
+      else
+        @conection.update_attributes status: false
       end
-    else
-      @conection.update_attributes status: false
     end
+
+    conections_respond
   end
 
   def destroy
     if @conection.present?
       @user =User.find_by id: @conection.recipient_id
       @conection.destroy
-      respond_to do |format|
-        format.html {redirect_to root_path}
-        format.js
-      end
+      conections_respond
     end
   end
 
@@ -54,5 +50,12 @@ class ConectionsController < ApplicationController
 
   def check_conected recipient_id
     current_user.conections.find_by recipient_id: recipient_id
+  end
+
+  def conections_respond
+    respond_to do |format|
+      format.html {redirect_to root_path}
+      format.js
+    end
   end
 end
